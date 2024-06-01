3f2a4aa1866d16321c93ab236dd81498da3564d4
Jayden <jaynor4t@gmail.com>
2024-06-01 15:50:03 +0800

    Added constructor to Star class

---
+++ Star.js
@@ -0,0 +1,12 @@
+class Star {
+    constructor(data) {
+        this.systemName = data["sy_name"]
+        this.hostname = data["hostname"]
+        this.stellarRadius = data["st_rad"]
+        this.stellarMass = data["st_mass"]
+        this.stellarDensity = data["st_dens"]
+        this.ra = data["ra"]
+        this.dec = data["dec"]
+        this.systemDistance = data["sy_dist"]
+    }
+}
