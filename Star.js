5fe1e7f59fb3cc54bea864431cf41ca56e1f93e8
Jayden <jaynor4t@gmail.com>
2024-06-02 17:29:36 +0800

    Added planets to Star constructor

--- Star.js
+++ Star.js
@@ -1,12 +1,13 @@
 class Star {
-    constructor(data) {
-        this.systemName = data["sy_name"]
-        this.hostname = data["hostname"]
-        this.stellarRadius = data["st_rad"]
-        this.stellarMass = data["st_mass"]
-        this.stellarDensity = data["st_dens"]
-        this.ra = data["ra"]
-        this.dec = data["dec"]
-        this.systemDistance = data["sy_dist"]
-    }
+  constructor(data, planets) {
+    this.systemName = data["sy_name"];
+    this.hostname = data["hostname"];
+    this.stellarRadius = data["st_rad"];
+    this.stellarMass = data["st_mass"];
+    this.stellarDensity = data["st_dens"];
+    this.ra = data["ra"];
+    this.dec = data["dec"];
+    this.systemDistance = data["sy_dist"];
+    this.planets = planets;
+  }
 }
