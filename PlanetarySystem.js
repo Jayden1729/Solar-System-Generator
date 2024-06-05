f7652f580ef571855db977d2d200bea0f49860b0
Jayden <jaynor4t@gmail.com>
2024-06-05 21:33:48 +0800

    Added method to find total star mass in system

--- PlanetarySystem.js
+++ PlanetarySystem.js
@@ -2,5 +2,27 @@ class PlanetarySystem {
   constructor(stars, planets, systems = null) {
     this.stars = stars;
     this.planets = planets;
+    this.systems = systems;
+    this.totalStarMass = getStarMass();
+  }
+  
+  
+  /**
+   * Calculates the total mass of stars in the system.
+   *
+   * @returns {number} - the total mass of stars in the system.
+   */
+  getStarMass() {
+    let totalStarMass = 0;
+
+    for (const star in this.stars) {
+      totalStarMass += star.stellarMass;
+    }
+
+    for (const system in this.systems) {
+      totalStarMass += system.totalStarMass;
+    }
+
+    return totalStarMass;
   }
 }
